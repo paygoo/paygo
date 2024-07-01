5d046683a80b23dbefa26dfca1112f368750be43
Darshan Kumar <itsdarshankumar@gmail.com>
2024-07-01 07:12:53 +0530

    superfluid ++
    Signed-off-by: Darshan Kumar <itsdarshankumar@gmail.com>

--- Backend/superfluid.js
+++ Backend/superfluid.js
@@ -38,3 +38,116 @@ async function createFlow(sender, privateKey_sender, receiver, rate) {
   const txnReceipt = await txnResponse.wait();
   return txnReceipt;
 }
+
+async function convertToSuperToken(privateKey_sender, amount) {
+  const provider = new ethers.providers.AlchemyProvider(
+    "base-sepolia",
+    process.env.ALCHEMY
+  );
+
+  const sf = await Framework.create({
+    chainId: 84532, // Replace with your chain ID
+    provider,
+  });
+  
+  const ethx = await sf.loadSuperToken("ETHx");
+
+  const signer = sf.createSigner({
+    privateKey: privateKey_sender,
+    provider,
+  });
+
+  const upgradeOperation = ethx.upgrade({
+    amount: ethers.utils.parseEther(amount), // Amount to upgrade
+  });
+
+  const txnResponse = await upgradeOperation.exec(signer);
+  const txnReceipt = await txnResponse.wait();
+  return txnReceipt;
+}
+
+async function sendSuperToken(privateKey_sender, receiver, amount) {
+  const provider = new ethers.providers.AlchemyProvider(
+    "base-sepolia",
+    process.env.ALCHEMY
+  );
+
+  const sf = await Framework.create({
+    chainId: 84532, // Replace with your chain ID
+    provider,
+  });
+
+  const ethx = await sf.loadSuperToken("ETHx");
+
+  const signer = sf.createSigner({
+    privateKey: privateKey_sender,
+    provider,
+  });
+
+  const sendOperation = ethx.transfer({
+    to: receiver, // Replace with the receiver's address
+    amount: ethers.utils.parseEther(amount), // Amount to send
+  });
+
+  const txnResponse = await sendOperation.exec(signer);
+  const txnReceipt = await txnResponse.wait();
+  return txnReceipt;
+}
+
+async function loadBalance(address) {
+  const provider = new ethers.providers.AlchemyProvider(
+    "base-sepolia",
+    process.env.ALCHEMY
+  );
+
+  const sf = await Framework.create({
+    chainId: 84532, // Replace with your chain ID
+    provider,
+  });
+
+  const ethx = await sf.loadSuperToken("ETHx");
+  const balance = await ethx.balanceOf(address);
+  return balance;
+}
+
+async function loadFlow(sender, receiver) {
+    const provider = new ethers.providers.AlchemyProvider(
+        "base-sepolia",
+        process.env.ALCHEMY
+    );
+    
+    const sf = await Framework.create({
+        chainId: 84532, // Replace with your chain ID
+        provider,
+    });
+    
+    const ethx = await sf.loadSuperToken("ETHx");
+    
+    const flowInfo = await ethx.getFlow({
+      sender: sender, // Replace with the sender's address
+      receiver: receiver, // Replace with the receiver's address
+      providerOrSigner: provider,
+    });
+    return flowInfo;
+}
+
+async function loadNetFlow(sender){
+    const provider = new ethers.providers.AlchemyProvider(
+        "base-sepolia",
+        process.env.ALCHEMY
+    );
+    
+    const sf = await Framework.create({
+        chainId: 84532, // Replace with your chain ID
+        provider,
+    });
+    
+    const ethx = await sf.loadSuperToken("ETHx");
+    
+    const netFlow = await ethx.getNetFlow({
+      account: sender, // Replace with the account address
+      token: ethx,
+      providerOrSigner: provider,
+    });
+    return netFlow;
+}
